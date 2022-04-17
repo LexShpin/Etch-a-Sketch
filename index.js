795a944aa513a8a91ef9bac93467b6da9bcfe866
lexshpin <lexshpin@gmail.com>
2022-04-17 09:09:39 -0600

    Generated the grid with hardcoded values, added coloring for grid elements on mouseover

---
+++ index.js
@@ -0,0 +1,31 @@
+const sizeSlider = document.querySelector('.size-slider');
+const grid = document.querySelector('.grid');
+
+function createGridElement() {
+    let gridElement = document.createElement('div');
+
+    gridElement.classList.add('grid-element');
+
+    gridElement.style.width = `${sizeSlider.value * 2}px`;
+    gridElement.style.height = `${sizeSlider.value * 2}px`;
+
+    gridElement.style.border = '1px solid black';
+
+    grid.appendChild(gridElement);
+
+    return;
+}
+
+for (let i = 1; i < sizeSlider.value; i++) {
+    
+    createGridElement();
+
+    for (let j = 1; j < sizeSlider.value; j++) {
+        createGridElement();
+    }
+}
+
+const gridElements = document.querySelectorAll('.grid-element');
+gridElements.forEach((element) => element.addEventListener('mouseover', () => {
+    element.style.backgroundColor = 'red';
+}));
